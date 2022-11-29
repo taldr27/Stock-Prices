b1bf67368a48f5f2896fb569d6cca38769cd4a2e
taldr27 <diegogarciaq98@gmail.com>
2022-11-29 11:20:06 -0500

    Add some styling

--- src/pages/Companies.js
+++ src/pages/Companies.js
@@ -4,9 +4,11 @@ import { IoMdSettings, IoIosArrowBack } from 'react-icons/io';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { fetchCompanies } from '../redux/Companies';
+import '../styles/Company.css';
 
 const Companies = () => {
   const companies = useSelector((state) => state.companies);
+  const details = useSelector((state) => state.details);
   const urlElement = window.location.href.split('/')[4];
   const dispatch = useDispatch();
   useEffect(() => {
@@ -21,13 +23,24 @@ const Companies = () => {
           <IoMdSettings />
         </div>
       </nav>
-      {companies.map((company) => (
-        <div key={company.symbol}>
-          <h2>{company.companyName}</h2>
-          <h2>{company.marketCap}</h2>
-          <NavLink to={`/companies/${urlElement}/${(company.symbol).replace(/\s/g, '')}/company-details`}>To Details</NavLink>
-        </div>
-      ))}
+      <div className="companies-container">
+        {details.map((detail) => (companies.map((company) => (
+          <div key={company.symbol} className="company-field">
+            <NavLink to={`/companies/${urlElement}/${(company.symbol).replace(/\s/g, '')}/company-details`}>
+              <div className="div-company">
+                <div>
+                  <img alt="company-img" src={detail.image} />
+                  <div className="name-market">
+                    <em>{company.companyName}</em>
+                    <em>{`Market Cap: $${(company.marketCap)}`}</em>
+                  </div>
+                </div>
+                <p>{`Price: ${detail.price}`}</p>
+              </div>
+            </NavLink>
+          </div>
+        ))))}
+      </div>
     </>
   );
 };
